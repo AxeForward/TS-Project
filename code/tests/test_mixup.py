dd6dfe3764b9ade0994acf275546957663e519e5
AxeForward <gamblingdoge@gmail.com>
2022-11-10 14:51:38 +0800

    Example testing structure

--- code/tests/test_mixup.py
+++ code/tests/test_mixup.py
@@ -12,12 +12,12 @@ def get_results_path()->os.PathLike:
 
 @pytest.mark.parametrize('Origin_Result, Project_Result', [('ACSF1_10epochs_42seed.csv', 'ucr_ACSF1_10epochs_42seed.csv')])
 def test_mix_up_ucr(get_results_path, Origin_Result, Project_Result):
-    origin_res_file = os.path.join(get_results_path, 'UCR', Origin_Result)
+    origin_res_file = os.path.join(get_results_path, 'mix_up', 'UCR', Origin_Result)
     df_origin = pd.read_csv(origin_res_file, index_col=0)
     origin_loss = df_origin['loss'].values
     origin_acc = df_origin['acc'].values
 
-    project_res_file = os.path.join(get_results_path, 'UCR', Project_Result)
+    project_res_file = os.path.join(get_results_path, 'mix_up', 'UCR', Project_Result)
     df_project = pd.read_csv(project_res_file, index_col=0)
     project_loss = df_project['loss'].values
     project_acc = df_project['acc'].values
