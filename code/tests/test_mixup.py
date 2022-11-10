3d6b5157bd1427b802c169cea5f6fd6472ec575f
AxeForward <gamblingdoge@gmail.com>
2022-11-10 11:45:33 +0800

    Tried to test mix up model

--- code/tests/test_mixup.py
+++ code/tests/test_mixup.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import pytest
 import os
 import typing
@@ -8,6 +10,18 @@ def get_results_path()->os.PathLike:
     results_dir = os.path.join(test_dir, 'results')
     return results_dir
 
-@pytest.mark.parametrize('UCR_Dataset_Name', ['GunPoint', 'Chinatown'])
-def test_mix_up_ucr(get_results_path, UCR_Dataset_Name):
-    res_path = os.path.join(get_results_path, 'UCR', UCR_Dataset_Name)
+@pytest.mark.parametrize('Origin_Result, Project_Result', [('ACSF1_10epochs_42seed.csv', 'ucr_ACSF1_10epochs_42seed.csv')])
+def test_mix_up_ucr(get_results_path, Origin_Result, Project_Result):
+    origin_res_file = os.path.join(get_results_path, 'UCR', Origin_Result)
+    df_origin = pd.read_csv(origin_res_file, index_col=0)
+    origin_loss = df_origin['loss'].values
+    origin_acc = df_origin['acc'].values
+
+    project_res_file = os.path.join(get_results_path, 'UCR', Project_Result)
+    df_project = pd.read_csv(project_res_file, index_col=0)
+    project_loss = df_project['loss'].values
+    project_acc = df_project['acc'].values
+
+    assert np.allclose(origin_loss, project_loss)
+    assert np.allclose(origin_acc, project_acc)
+
