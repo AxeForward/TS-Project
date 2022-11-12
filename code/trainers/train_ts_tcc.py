ca4f765049cd63b93620ecc8e914ae649d51145b
fintech-cai <cai_jingcheng@126.com>
2022-11-12 10:46:49 +0800

    modify train_ts_tcc

--- code/trainers/train_ts_tcc.py
+++ code/trainers/train_ts_tcc.py
@@ -11,81 +11,93 @@ from utils import DataTransform
 
 
 
-def Trainer(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, train_dl, valid_dl, test_dl, device,num_epoch, logger,experiment_log_dir,training_mode,jitter_scale_ratio,jitter_ratio,max_seg,batch_size, temperature=0.2,use_cosine_similarity=True):
+def Trainer(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, train_dl, valid_dl, test_dl, device, logger, config, experiment_log_dir, training_mode):
     # Start training
-    #logger.debug("Training started ....")
+    # logger.debug("Training started ....")
 
     criterion = nn.CrossEntropyLoss()
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(model_optimizer, 'min')
 
-    for epoch in range(1, num_epoch + 1):
+    LossList, AccList = [] , []
+    for epoch in range(1, config.num_epoch + 1):
         # Train and validate
-        train_loss, train_acc = model_train(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, criterion, train_dl, device, training_mode,
-                                                jitter_scale_ratio,jitter_ratio,max_seg,batch_size, temperature,use_cosine_similarity)
+        train_loss, train_acc = model_train(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, criterion, train_dl, config, device, training_mode)
         valid_loss, valid_acc, _, _ = model_evaluate(model, temporal_contr_model, valid_dl, device, training_mode)
+        LossList.append(train_loss.item())
+        AccList.append(train_acc.item())
         if training_mode != 'self_supervised':  # use scheduler in all other modes.
             scheduler.step(valid_loss)
 
-        logger.debug(f'\nEpoch : {epoch}\n'
-                     f'Train Loss     : {train_loss:.4f}\t | \tTrain Accuracy     : {train_acc:2.4f}\n'
-                     f'Valid Loss     : {valid_loss:.4f}\t | \tValid Accuracy     : {valid_acc:2.4f}')
+        # logger.debug(f'\nEpoch : {epoch}\n'
+        #              f'Train Loss     : {train_loss:.4f}\t | \tTrain Accuracy     : {train_acc:2.4f}\n'
+        #              f'Valid Loss     : {valid_loss:.4f}\t | \tValid Accuracy     : {valid_acc:2.4f}')
 
     os.makedirs(os.path.join(experiment_log_dir, "saved_models"), exist_ok=True)
     chkpoint = {'model_state_dict': model.state_dict(), 'temporal_contr_model_state_dict': temporal_contr_model.state_dict()}
     torch.save(chkpoint, os.path.join(experiment_log_dir, "saved_models", f'ckp_last.pt'))
 
+
+
+
     if training_mode != "self_supervised":  # no need to run the evaluation for self-supervised mode.
         # evaluate on the test set
-        logger.debug('\nEvaluate on the Test set:')
+        # logger.debug('\nEvaluate on the Test set:')
         test_loss, test_acc, _, _ = model_evaluate(model, temporal_contr_model, test_dl, device, training_mode)
-        logger.debug(f'Test loss      :{test_loss:0.4f}\t | Test Accuracy      : {test_acc:0.4f}')
+        # logger.debug(f'Test loss      :{test_loss:0.4f}\t | Test Accuracy      : {test_acc:0.4f}')
 
-    logger.debug("\n################## Training is Done! #########################")
+    return LossList, AccList
+    # logger.debug("\n################## Training is Done! #########################")
 
 
-def model_train(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, criterion, train_loader, device, training_mode,
-                jitter_scale_ratio,jitter_ratio,max_seg,batch_size, temperature,use_cosine_similarity):
+def model_train(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, criterion, train_loader, config, device, training_mode):
     total_loss = []
     total_acc = []
     model.train()
     temporal_contr_model.train()
 
+
     for batch_idx, (data, labels, aug1, aug2) in enumerate(train_loader):
         # send to device
-        aug1,aug2 = DataTransform(data,jitter_scale_ratio,jitter_ratio,max_seg)
         data, labels = data.float().to(device), labels.long().to(device)
-        aug1 = torch.from_numpy(aug1)
         aug1, aug2 = aug1.float().to(device), aug2.float().to(device)
+
         # optimizer
         model_optimizer.zero_grad()
         temp_cont_optimizer.zero_grad()
 
+
+
+
         if training_mode == "self_supervised":
             predictions1, features1 = model(aug1)
             predictions2, features2 = model(aug2)
-      
+
             # normalize projection feature vectors
             features1 = F.normalize(features1, dim=1)
             features2 = F.normalize(features2, dim=1)
 
-            temp_cont_loss1, temp_cont_lstm_feat1,c_tokens1,fai_l1,fai_01,z_h1,c_t1,forward_seq1,encode_samples1,pred1= temporal_contr_model(features1, features2)
-            temp_cont_loss2, temp_cont_lstm_feat2,c_tokens2,fai_l2,fai_02,z_h2,c_t2,forward_seq2,encode_samples,pred2 = temporal_contr_model(features2, features1)
+            temp_cont_loss1, temp_cont_lstm_feat1 = temporal_contr_model(features1, features2)
+            temp_cont_loss2, temp_cont_lstm_feat2 = temporal_contr_model(features2, features1)
 
             # normalize projection feature vectors
-            zis = temp_cont_lstm_feat1 
-            zjs = temp_cont_lstm_feat2 
+            zis = temp_cont_lstm_feat1
+            zjs = temp_cont_lstm_feat2
+
+
+
 
         else:
             output = model(data)
+
 
         # compute loss
         if training_mode == "self_supervised":
             lambda1 = 1
             lambda2 = 0.7
-            nt_xent_criterion = NTXentLoss(device, batch_size, temperature,
-                                           use_cosine_similarity)
+            nt_xent_criterion = NTXentLoss(device, config.batch_size, config.Context_Cont.temperature,
+                                           config.Context_Cont.use_cosine_similarity)
             loss = (temp_cont_loss1 + temp_cont_loss2) * lambda1 +  nt_xent_criterion(zis, zjs) * lambda2
-            
+
         else: # supervised training or fine tuining
             predictions, features = output
             loss = criterion(predictions, labels)
@@ -147,5 +159,3 @@ def model_evaluate(model, temporal_contr_model, test_dl, device, training_mode):
     else:
         total_acc = torch.tensor(total_acc).mean()  # average acc
     return total_loss, total_acc, outs, trgs
-
-
