bef467c20a534796ad4c81858fe29c0dccb50074
AxeForward <gamblingdoge@gmail.com>
2022-11-21 11:16:39 +0800

    Changed origin result to docker result

--- code/tests/test_mixup.py
+++ code/tests/test_mixup.py
@@ -16,7 +16,7 @@ def get_results_path()->os.PathLike:
     return results_dir
 
 @pytest.mark.parametrize('Origin_Result',
-                         ['GunPoint_10epochs_42seed.csv', 'notebookGunPoint_10epochs_42seed.csv'])
+                         ['docker_notebookGunPoint_10epochs_42seed.csv'])
 def test_mix_up_ucr(get_results_path, Origin_Result):
     origin_res_file = os.path.join(get_results_path, 'mix_up', 'UCR', Origin_Result)
     df_origin = pd.read_csv(origin_res_file, index_col=0)
